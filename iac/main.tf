56532f5fdee43be4044032e396b6835b1eaa93ed
Nelly-98 <nelly.guepnang@gmail.com>
2024-03-27 12:37:50 +0100

    modified terraform ressources

--- iac/main.tf
+++ iac/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 module "vpc" {
   source               = "./modules/vpc"
-  project_name         = "vpc-poei-web1"
+  project_name         = "poei-web1"
   cidr                 = "10.0.0.0/16"
   azs                  = ["eu-west-3a", "eu-west-3b"]
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -32,7 +32,7 @@ module "vpc" {
 
 module "rds" {
   source                 = "./modules/rds"
-  project_name           = "rds-poei-web1"
+  project_name           = "poei-web1"
   instance_class         = "db.t3.micro"
   engine                 = "mysql"
   engine_version         = "8.0.28"
@@ -42,20 +42,6 @@ module "rds" {
   password               = "unMotDePasse"
   subnet_group_name      = module.vpc.rds_subnet_group_name
   vpc_security_group_ids = [module.sg.security_group_id] # Le groupe de sécurité va permettre les connexions à RDS 
-}
-
-module "eks" {
-  source                = "./modules/eks"
-  cluster_name          = "eks-poie-web1"
-  subnets               = module.vpc.public_subnets_ids
-  vpc_id                = module.vpc.vpc_id
-  node_group_name       = "poei-group-de-nodes"
-  node_instance_type    = "t3.medium"
-  node_desired_capacity = 2
-  node_max_capacity     = 3
-  node_min_capacity     = 1
-  cluster_iam_role_arn  = "arn:aws:iam::885801475464:role/EKSClusterRole"
-  node_iam_role_arn     = "arn:aws:iam::885801475464:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
 }
 
 module "ecr" {
@@ -69,6 +55,74 @@ module "sg" {
   vpc_id      = module.vpc.vpc_id
   name        = "sg-rds"
   description = "Security Group pour les instances RDS"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "eks-poie-web1"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets_ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    example = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::885801475464:role/EKSClusterRole"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
 
